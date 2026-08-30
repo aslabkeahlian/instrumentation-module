bb19ff7b09cc4a9b072ca5056ae9dd47e7a887b2
aslabkeahlian <aslabkeahlian@gmail.com>
2026-08-30 19:57:55 +0700

    Update Modul8-TesLCD-Keahlian.ino

--- Modul8-TesLCD-Keahlian.ino
+++ Modul8-TesLCD-Keahlian.ino
@@ -1,21 +1,16 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
-LiquidCrystal_I2C lcd(0x20, 16, 2);    // Untuk di protheus pakai 20
+LiquidCrystal_I2C lcd(0x20, 16, 2);    // Untuk di protheus pakai LCD Address 20 dan 27 untuk di percobaan realtimenya
 
 void setup() {
   lcd.init();
   lcd.backlight();
-
-  // Tampilan Baris 1
   lcd.setCursor(0, 0);
   lcd.print("LAB.......FISIKA");
-
-  // Tampilan Baris 2
   lcd.setCursor(0, 1);
   lcd.print("..INSTRUMENTASI.");
 }
 
 void loop() {
-  // Kosong karena tampilan statis
 }
