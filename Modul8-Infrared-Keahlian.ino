fc5ccec4a04bbbb5725c38cce8a0fc98b0e2a930
aslabkeahlian <aslabkeahlian@gmail.com>
2026-09-02 11:34:38 +0700

    Create Modul8-Infrared-Keahlian.ino

---
+++ Modul8-Infrared-Keahlian.ino
@@ -0,0 +1,33 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x20, 16, 2); // Untuk di protheus pakai LCD Address 20 dan 27 untuk di percobaan realtimenya
+
+void setup() {
+  pinMode(7, INPUT);
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+  lcd.print("..INFRARED..");
+  lcd.setCursor(0, 1);
+  lcd.print("....SIMULASI....");
+  delay(1000);
+  lcd.clear();
+}
+
+void loop() {
+  byte sensorValue = digitalRead(7);
+  lcd.setCursor(0, 0);
+  if (sensorValue == HIGH) {
+    lcd.print("...TERDETEKSI...");
+    lcd.setCursor(0, 1);
+    lcd.print(".....OBJEK......");
+  } else {
+    lcd.print(".TDK TERDETEKSI.");
+    lcd.setCursor(0, 1);
+    lcd.print(".....OBJEK......");
+  }
+
+  delay(500);
+  lcd.clear();
+}
