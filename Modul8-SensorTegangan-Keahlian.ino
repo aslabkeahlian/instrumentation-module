060b9db029165f4aa46622fb9fddfc78aada85f7
aslabkeahlian <aslabkeahlian@gmail.com>
2026-09-02 14:41:02 +0700

    Update Modul8-SensorTegangan-Keahlian.ino

--- Modul8-SensorTegangan-Keahlian.ino
+++ Modul8-SensorTegangan-Keahlian.ino
@@ -1,7 +1,7 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
-LiquidCrystal_I2C lcd(0x20, 16, 2);  // Untuk di protheus pakai LCD Address 20 dan 27 untuk di percobaan realtimenya
+LiquidCrystal_I2C lcd(0x20, 16, 2); // Untuk di protheus pakai LCD Address 20 dan 27 untuk di percobaan realtimenya
 
 const int analogIn = A0;
 
@@ -19,13 +19,13 @@ void setup() {
 
 void loop() {
   int rawValue = analogRead(analogIn);
-  float voltage = rawValue * (5.0 / 1023.0);
+  float voltage = (rawValue * (5.0 / 1024.0) * ((30.0 + 7.5) / 7.5));
   lcd.setCursor(0, 0);
   lcd.print("SENSOR TEGANGAN ");
   lcd.setCursor(0, 1);
   lcd.print("Teg = ");
   lcd.setCursor(6, 1);
-  lcd.print(voltage, 2); // Menampilkan tegangan dengan 2 angka desimal
+  lcd.print(voltage, 2); 
   lcd.setCursor(15, 1);
   lcd.print("V");
   delay(1000);
