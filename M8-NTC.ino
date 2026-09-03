2199220e9ecbb49ad4a989551c8c220296c7a2ce
aslabkeahlian <aslabkeahlian@gmail.com>
2026-09-03 14:48:01 +0700

    .

--- M8-NTC.ino
+++ M8-NTC.ino
@@ -8,7 +8,7 @@ void setup() {
   lcd.init();
   lcd.backlight();
   lcd.setCursor(0, 0);
-  lcd.print("..NTC SENSOR..");
+  lcd.print("..SENSOR NTC..");
   lcd.setCursor(0, 1);
   lcd.print("....SIMULASI....");
   delay(1000);
