2d14bca8a53be87b934c15b0ba61b4bd585e8c66
aslabkeahlian <aslabkeahlian@gmail.com>
2026-09-15 11:08:53 +0700

    .

--- M9-Relay.ino
+++ M9-Relay.ino
@@ -1,7 +1,7 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
-LiquidCrystal_I2C lcd(0x20, 16, 2); 
+LiquidCrystal_I2C lcd(0x20, 16, 2); // Untuk di protheus pakai LCD Address 20 dan 27 untuk di percobaan realtimenya
 
 void setup() {
   pinMode(13, OUTPUT)
@@ -18,7 +18,7 @@ void setup() {
 void loop() {
   digitalWrite(13, HIGH);
   lcd.setCursor(0, 0);
-  lcd.print("--...RELAY....--");
+  lcd.print(" ....RELAY.... ");
   lcd.setCursor(0, 1);
   lcd.print(".....AKTIF......");
   delay(3000);
@@ -26,7 +26,7 @@ void loop() {
 
   digitalWrite(13, LOW);
   lcd.setCursor(0, 0);
-  lcd.print("--...RELAY....--");
+  lcd.print(" ....RELAY.... ");
   lcd.setCursor(0, 1);
   lcd.print("...TDK AKTIF....");
   delay(3000);
