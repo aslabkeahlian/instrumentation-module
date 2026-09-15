d8c26725a717fd0782cd70a3490d57ea98a382ad
aslabkeahlian <aslabkeahlian@gmail.com>
2026-09-15 11:34:03 +0700

    .

---
+++ M9-MotorServo.ino
@@ -0,0 +1,34 @@
+#include <Servo.h>
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x20,16,2);  // Untuk di protheus pakai LCD Address 20 dan 27 untuk di percobaan realtimeny
+Servo myservo;
+int val =0;
+int Servo_value,Servo_value1 =0;
+
+void setup() {
+  myservo.attach(9);
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(0,0);
+  lcd.print("--MOTOR SERVO---");
+  lcd.setCursor(0,1);
+  lcd.print("    SIMULASI    ");
+  delay(1000);
+  lcd.clear();
+}
+void loop() {
+  val = analogRead(A0);
+  Servo_value = map(val,0,1023,44,142);
+  Servo_value1= map(Servo_value,44,142,0,180);
+  myservo.write(Servo_value ); 
+  lcd.setCursor(0,0);
+  lcd.print("--MOTOR SERVO---");
+  lcd.setCursor(0,1);
+  lcd.print("Servo = ");
+  lcd.setCursor(7,1);
+  lcd.print(Servo_value1);
+  delay(500);
+  lcd.clear();
+}
