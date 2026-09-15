8771a5792f8de2218cb90787a71df5ac3a144500
aslabkeahlian <aslabkeahlian@gmail.com>
2026-09-15 11:05:40 +0700

    .

---
+++ M9-Relay.ino
@@ -0,0 +1,34 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x20, 16, 2); 
+
+void setup() {
+  pinMode(13, OUTPUT)
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+  lcd.print(" AKTUATOR RELAY ");
+  lcd.setCursor(0, 1);
+  lcd.print("....SIMULASI....");
+  delay(1000);
+  lcd.clear();
+}
+
+void loop() {
+  digitalWrite(13, HIGH);
+  lcd.setCursor(0, 0);
+  lcd.print("--...RELAY....--");
+  lcd.setCursor(0, 1);
+  lcd.print(".....AKTIF......");
+  delay(3000);
+  lcd.clear();
+
+  digitalWrite(13, LOW);
+  lcd.setCursor(0, 0);
+  lcd.print("--...RELAY....--");
+  lcd.setCursor(0, 1);
+  lcd.print("...TDK AKTIF....");
+  delay(3000);
+  lcd.clear();
+}
