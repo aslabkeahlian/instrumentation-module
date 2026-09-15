54159f03d1595e7c8894804e0ad6e4d21048dfa2
aslabkeahlian <aslabkeahlian@gmail.com>
2026-09-15 22:27:40 +0700

    .

--- M8-TesLCD.ino
+++ M8-TesLCD.ino
@@ -3,14 +3,14 @@
 
 LiquidCrystal_I2C lcd(0x20, 16, 2);    // Untuk di protheus pakai LCD Address 20 dan 27 untuk di percobaan realtimenya
 
-// Ini untuk tampilan awal LCD
+// Ini untuk tampilan awal LCD, hanya bisa sampai 16 karakter huruf
 void setup() {
   lcd.init();
   lcd.backlight();
   lcd.setCursor(0, 0);
-  lcd.print(" PRAK KEAHLIAN  ");
+  lcd.print(" PRAK KEAHLIAN  ");    // Ganti tampilan awal baris pertama sesuai kemauan kalian
   lcd.setCursor(0, 1);
-  lcd.print(" INSTRUMENTASI  ");
+  lcd.print(" INSTRUMENTASI  ");    // Ini untuk tampilan baris keduanya
 }
 
 void loop() {
