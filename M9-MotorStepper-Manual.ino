a7217bd56a38857e6e7b8f8a13a4bf761fd130bb
aslabkeahlian <aslabkeahlian@gmail.com>
2026-09-22 14:36:57 +0700

    .

--- M9-MotorStepper-Manual.ino
+++ M9-MotorStepper-Manual.ino
@@ -1,184 +1,125 @@
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
 
-LiquidCrystal_I2C lcd(0x27,16,2);  
-int koil1 =11;
-int koil2 =10;
-int koil3 =9;
-int koil4 =8;
-int i, x=0,z=0, y;
-const int period =15;
-float putaran1,putaran2 ;
-int putar = 2;
+LiquidCrystal_I2C lcd(0x27, 16, 2);  
+int koil1 = 11;
+int koil2 = 10;
+int koil3 = 9;
+int koil4 = 8;
+int i, x = 0, z = 0, y;
+const int period = 15;
+float putaran1, putaran2;
+
+int putar = 2; // Jumlah putaran yang diinginkan
 
 void setup() {
-  pinMode(koil1,OUTPUT);
-  pinMode(koil2,OUTPUT);
-  pinMode(koil3,OUTPUT);
-  pinMode(koil4,OUTPUT);
+  pinMode(koil1, OUTPUT);
+  pinMode(koil2, OUTPUT);
+  pinMode(koil3, OUTPUT);
+  pinMode(koil4, OUTPUT);
+  
   lcd.init();
   lcd.backlight();
-  lcd.setCursor(0,0);
+  lcd.setCursor(0, 0);
   lcd.print("LAB.......FISIKA");
-  lcd.setCursor(0,1);
+  lcd.setCursor(0, 1);
   lcd.print("..INSTRUMENTASI.");
   delay(1000);
   lcd.clear();
 }
+
 void loop() {
-y = putar*512;
-stepper_arah_jarum_jam();
-delay(2000);
-lcd.clear();
-stepper_lawan_arah_jarum_jam();
-delay(2000);
-lcd.clear();
-if ((x==y) && (z==y)){
-putar = 0;  }
+  // Hanya jalankan jika putar > 0
+  if (putar > 0) {
+    y = putar * 512;
+    
+    stepper_arah_jarum_jam();
+    delay(2000);
+    lcd.clear();
+    
+    stepper_lawan_arah_jarum_jam();
+    delay(2000);
+    lcd.clear();
+    
+    // Logika untuk memberhentikan mesin setelah 1 siklus (CW -> CCW)
+    if ((x == y) && (z == y)) {
+      putar = 0;  // Mesin berhenti setelah ini
+    }
+    
+    /* 
+    CATATAN: Jika Anda ingin motor memutar TERUS MENERUS (bolak-balik), 
+    hapus/jadikan komentar blok "if ((x == y)..." di atas, lalu aktifkan 
+    dua baris di bawah ini untuk mereset counter:
+    */
+    // x = 0;
+    // z = 0;
+  }
 }
 
-void stepper_arah_jarum_jam(){
-  lcd.setCursor(0,0);
+void stepper_arah_jarum_jam() {
+  lcd.setCursor(0, 0);
   lcd.print("ARAH +");
-  lcd.setCursor(7,0);
+  lcd.setCursor(7, 0);
   lcd.print("SP =");
-  lcd.setCursor(11,0);
-  lcd.print(putar,1);
-  for(i=0;i<y;i++){
-  x++;
-  //step 1 ( 0x01H  biner 1000)
-  digitalWrite(koil1,HIGH);
-  digitalWrite(koil2,LOW);
-  digitalWrite(koil3,LOW);
-  digitalWrite(koil4,LOW);
-  delay(period);
-  //step2 ( 0x03H biner 1100)
-  digitalWrite(koil1,HIGH);
-  digitalWrite(koil2,HIGH);
-  digitalWrite(koil3,LOW);
-  digitalWrite(koil4,LOW);
-  delay(period);
-  //step3 (0x02H biner 0100)
-  digitalWrite(koil1,LOW);
-  digitalWrite(koil2,HIGH);
-  digitalWrite(koil3,LOW);
-  digitalWrite(koil4,LOW);
-  delay(period);
-  //step4 (0x06H biner 0110)
-  digitalWrite(koil1,LOW);
-  digitalWrite(koil2,HIGH);
-  digitalWrite(koil3,HIGH);
-  digitalWrite(koil4,LOW);
-  delay(period);
-  //step5 (0x04H biner 0010)
-  digitalWrite(koil1,LOW);
-  digitalWrite(koil2,LOW);
-  digitalWrite(koil3,HIGH);
-  digitalWrite(koil4,LOW);
-  delay(period);
-  //step6 (0x0CH biner 0011)
-  digitalWrite(koil1,LOW);
-  digitalWrite(koil2,LOW);
-  digitalWrite(koil3,HIGH);
-  digitalWrite(koil4,HIGH);
-  delay(period);
-  //step7 (0x08H biner 0001)
-  digitalWrite(koil1,LOW);
-  digitalWrite(koil2,LOW);
-  digitalWrite(koil3,LOW);
-  digitalWrite(koil4,HIGH);
-  delay(period);
-  //step8 (0x09 biner 1001)
-  digitalWrite(koil1,HIGH);
-  digitalWrite(koil2,LOW);
-  digitalWrite(koil3,LOW);
-  digitalWrite(koil4,HIGH);
-  delay(period);
+  lcd.setCursor(11, 0);
+  lcd.print(putar); // Perbaikan: hilangkan angka ,1
+
+  for (i = 0; i < y; i++) {
+    x++;
+    digitalWrite(koil1, HIGH); digitalWrite(koil2, LOW); digitalWrite(koil3, LOW); digitalWrite(koil4, LOW); delay(period);
+    digitalWrite(koil1, HIGH); digitalWrite(koil2, HIGH); digitalWrite(koil3, LOW); digitalWrite(koil4, LOW); delay(period);
+    digitalWrite(koil1, LOW); digitalWrite(koil2, HIGH); digitalWrite(koil3, LOW); digitalWrite(koil4, LOW); delay(period);
+    digitalWrite(koil1, LOW); digitalWrite(koil2, HIGH); digitalWrite(koil3, HIGH); digitalWrite(koil4, LOW); delay(period);
+    digitalWrite(koil1, LOW); digitalWrite(koil2, LOW); digitalWrite(koil3, HIGH); digitalWrite(koil4, LOW); delay(period);
+    digitalWrite(koil1, LOW); digitalWrite(koil2, LOW); digitalWrite(koil3, HIGH); digitalWrite(koil4, HIGH); delay(period);
+    digitalWrite(koil1, LOW); digitalWrite(koil2, LOW); digitalWrite(koil3, LOW); digitalWrite(koil4, HIGH); delay(period);
+    digitalWrite(koil1, HIGH); digitalWrite(koil2, LOW); digitalWrite(koil3, LOW); digitalWrite(koil4, HIGH); delay(period);
   }
-    putaran1 = (x/512);
-  lcd.setCursor(0,1);
-  lcd.print("PUTARAN = ");
-  lcd.setCursor(10,1);
+  
+  // Perbaikan: gunakan casting float agar pembagian terbaca sebagai desimal
+  putaran1 = (float)x / 512.0; 
+  lcd.setCursor(0, 1);
+  lcd.print("PUTARAN= ");
   lcd.print(putaran1);
-  digitalWrite(koil1,HIGH);
-  digitalWrite(koil2,LOW);
-  digitalWrite(koil3,LOW);
-  digitalWrite(koil4,HIGH);
-  delay(period);
+  
+  matikan_koil(); // Matikan koil agar motor tidak panas saat jeda
   delay(3000);
 }
 
-void stepper_lawan_arah_jarum_jam(){
-  lcd.setCursor(0,0);
+void stepper_lawan_arah_jarum_jam() {
+  lcd.setCursor(0, 0);
   lcd.print("ARAH -");
-  lcd.setCursor(7,0);
+  lcd.setCursor(7, 0);
   lcd.print("SP =");
-  lcd.setCursor(11,0);
-  lcd.print(putar,1);
-  lcd.setCursor(0,1);
-  for(i=0;i<y;i++){
-  z++;
-  //step 1 ( 0x09H  biner 1001)
-  digitalWrite(koil1,HIGH);
-  digitalWrite(koil2,LOW);
-  digitalWrite(koil3,LOW);
-  digitalWrite(koil4,HIGH);
-  delay(period);
-  //step2 ( 0x08H biner 0001)
-  digitalWrite(koil1,LOW);
-  digitalWrite(koil2,LOW);
-  digitalWrite(koil3,LOW);
-  digitalWrite(koil4,HIGH);
-  delay(period);
-  //step3 (0x0CH biner 0011)
-  digitalWrite(koil1,LOW);
-  digitalWrite(koil2,LOW);
-  digitalWrite(koil3,HIGH);
-  digitalWrite(koil4,HIGH);
-  delay(period);
-  //step4 (0x04H biner 0010)
-  digitalWrite(koil1,LOW);
-  digitalWrite(koil2,LOW);
-  digitalWrite(koil3,HIGH);
-  digitalWrite(koil4,LOW);
-  delay(period);
-  //step5 (0x06H biner 0110)
-  digitalWrite(koil1,LOW);
-  digitalWrite(koil2,HIGH);
-  digitalWrite(koil3,HIGH);
-  digitalWrite(koil4,LOW);
-  delay(period);
-  //step6 (0x02H biner 0100)
-  digitalWrite(koil1,LOW);
-  digitalWrite(koil2,HIGH);
-  digitalWrite(koil3,LOW);
-  digitalWrite(koil4,LOW);
-  delay(period);
-  //step7 (0x03H biner 1100)
-  digitalWrite(koil1,HIGH);
-  digitalWrite(koil2,HIGH);
-  digitalWrite(koil3,LOW);
-  digitalWrite(koil4,LOW);
-  delay(period);
-  //step8 (0x01 biner 1000)
-  digitalWrite(koil1,HIGH);
-  digitalWrite(koil2,LOW);
-  digitalWrite(koil3,LOW);
-  digitalWrite(koil4,LOW);
-  delay(period);
+  lcd.setCursor(11, 0);
+  lcd.print(putar); // Perbaikan: hilangkan angka ,1
+  
+  for (i = 0; i < y; i++) {
+    z++;
+    digitalWrite(koil1, HIGH); digitalWrite(koil2, LOW); digitalWrite(koil3, LOW); digitalWrite(koil4, HIGH); delay(period);
+    digitalWrite(koil1, LOW); digitalWrite(koil2, LOW); digitalWrite(koil3, LOW); digitalWrite(koil4, HIGH); delay(period);
+    digitalWrite(koil1, LOW); digitalWrite(koil2, LOW); digitalWrite(koil3, HIGH); digitalWrite(koil4, HIGH); delay(period);
+    digitalWrite(koil1, LOW); digitalWrite(koil2, LOW); digitalWrite(koil3, HIGH); digitalWrite(koil4, LOW); delay(period);
+    digitalWrite(koil1, LOW); digitalWrite(koil2, HIGH); digitalWrite(koil3, HIGH); digitalWrite(koil4, LOW); delay(period);
+    digitalWrite(koil1, LOW); digitalWrite(koil2, HIGH); digitalWrite(koil3, LOW); digitalWrite(koil4, LOW); delay(period);
+    digitalWrite(koil1, HIGH); digitalWrite(koil2, HIGH); digitalWrite(koil3, LOW); digitalWrite(koil4, LOW); delay(period);
+    digitalWrite(koil1, HIGH); digitalWrite(koil2, LOW); digitalWrite(koil3, LOW); digitalWrite(koil4, LOW); delay(period);
   }
-  putaran2 = (z/512);
-  lcd.setCursor(0,1);
-  lcd.print("PUTARAN = ");
-  lcd.setCursor(10,1);
+  
+  // Perbaikan: gunakan casting float
+  putaran2 = (float)z / 512.0;
+  lcd.setCursor(0, 1);
+  lcd.print("PUTARAN= ");
   lcd.print(putaran2);
-  digitalWrite(koil1,HIGH);
-  digitalWrite(koil2,LOW);
-  digitalWrite(koil3,LOW);
-  digitalWrite(koil4,LOW);
-  delay(period);
+  
+  matikan_koil(); // Matikan koil agar motor tidak panas saat jeda
   delay(3000);
 }
 
-
-
+// Fungsi tambahan untuk mematikan semua listrik ke motor (Mencegah Overheat)
+void matikan_koil() {
+  digitalWrite(koil1, LOW);
+  digitalWrite(koil2, LOW);
+  digitalWrite(koil3, LOW);
+  digitalWrite(koil4, LOW);
+}
