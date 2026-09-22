81740b9c3096c42f6e2c136ec7aa851f8ef7e336
aslabkeahlian <aslabkeahlian@gmail.com>
2026-09-22 18:06:29 +0700

    .

--- M9-MotorStepper-Manual.ino
+++ M9-MotorStepper-Manual.ino
@@ -7,10 +7,10 @@ int koil2 = 10;
 int koil3 = 9;
 int koil4 = 8;
 int i, x = 0, z = 0, y;
-const int period = 15;
+const int period = 15; // ini nilainya semakin kecil makin cepet geraknya, BATASNYA SAMPE 2 
 float putaran1, putaran2;
 
-int putar = 2; // Jumlah putaran yang diinginkan
+int putar = 2; // diganti jadi mau berapa putaran 
 
 void setup() {
   pinMode(koil1, OUTPUT);
@@ -29,7 +29,6 @@ void setup() {
 }
 
 void loop() {
-  // Hanya jalankan jika putar > 0
   if (putar > 0) {
     y = putar * 512;
     
@@ -41,18 +40,9 @@ void loop() {
     delay(2000);
     lcd.clear();
     
-    // Logika untuk memberhentikan mesin setelah 1 siklus (CW -> CCW)
     if ((x == y) && (z == y)) {
-      putar = 0;  // Mesin berhenti setelah ini
+      putar = 0;
     }
-    
-    /* 
-    CATATAN: Jika Anda ingin motor memutar TERUS MENERUS (bolak-balik), 
-    hapus/jadikan komentar blok "if ((x == y)..." di atas, lalu aktifkan 
-    dua baris di bawah ini untuk mereset counter:
-    */
-    // x = 0;
-    // z = 0;
   }
 }
 
@@ -62,7 +52,7 @@ void stepper_arah_jarum_jam() {
   lcd.setCursor(7, 0);
   lcd.print("SP =");
   lcd.setCursor(11, 0);
-  lcd.print(putar); // Perbaikan: hilangkan angka ,1
+  lcd.print(putar); 
 
   for (i = 0; i < y; i++) {
     x++;
@@ -76,13 +66,12 @@ void stepper_arah_jarum_jam() {
     digitalWrite(koil1, HIGH); digitalWrite(koil2, LOW); digitalWrite(koil3, LOW); digitalWrite(koil4, HIGH); delay(period);
   }
   
-  // Perbaikan: gunakan casting float agar pembagian terbaca sebagai desimal
   putaran1 = (float)x / 512.0; 
   lcd.setCursor(0, 1);
   lcd.print("PUTARAN= ");
   lcd.print(putaran1);
   
-  matikan_koil(); // Matikan koil agar motor tidak panas saat jeda
+  matikan_koil(); 
   delay(3000);
 }
 
@@ -92,7 +81,7 @@ void stepper_lawan_arah_jarum_jam() {
   lcd.setCursor(7, 0);
   lcd.print("SP =");
   lcd.setCursor(11, 0);
-  lcd.print(putar); // Perbaikan: hilangkan angka ,1
+  lcd.print(putar);
   
   for (i = 0; i < y; i++) {
     z++;
@@ -106,17 +95,15 @@ void stepper_lawan_arah_jarum_jam() {
     digitalWrite(koil1, HIGH); digitalWrite(koil2, LOW); digitalWrite(koil3, LOW); digitalWrite(koil4, LOW); delay(period);
   }
   
-  // Perbaikan: gunakan casting float
   putaran2 = (float)z / 512.0;
   lcd.setCursor(0, 1);
   lcd.print("PUTARAN= ");
   lcd.print(putaran2);
   
-  matikan_koil(); // Matikan koil agar motor tidak panas saat jeda
+  matikan_koil();
   delay(3000);
 }
 
-// Fungsi tambahan untuk mematikan semua listrik ke motor (Mencegah Overheat)
 void matikan_koil() {
   digitalWrite(koil1, LOW);
   digitalWrite(koil2, LOW);
